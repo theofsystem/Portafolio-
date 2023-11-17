5a909fdff5e3b8e4064a1568dd8546fef79f9231
theofsystem <90411528+theofsystem@users.noreply.github.com>
2023-11-17 16:42:14 -0600

    Delete JavaScript directory

--- JavaScript/index.js
+++
@@ -1,6 +0,0 @@
-const btntoggle = document.querySelector(".btn");
-const menu = document.querySelector(".nav");
-
-btntoggle.addEventListener("click", () => {
-    menu.classList.toggle("nav-visibility");
-});
